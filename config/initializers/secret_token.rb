b748b405408caec7f6f4544d04ea5b68e0df49aa
Thomas Chen <foxnewsnetwork@gmail.com>
2011-10-03 21:41:48 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gossip::Application.config.secret_token = '6e8364a54bcad77da8e5798b26c89807e2225f2c5d839a7d992ed20ee82c3e284d1121e6f5dbfb920f8b222a670eddefaa144913b9022a8a37dd5d313b90a07c'

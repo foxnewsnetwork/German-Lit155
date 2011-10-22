70b46f465af573f7e695173c9f62735579e34765
Han Jing <hanqijing@gmail.com>
2011-10-21 19:29:27 -0700

    upgraded user login/logout environment. added toolbar. user model still needs integration with microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Gossip::Application.routes.draw do
 
   match 'user/edit' => 'users#edit', :as => :edit_current_user
-
   match 'signup' => 'users#new', :as => :signup
-
   match 'logout' => 'sessions#destroy', :as => :logout
-
   match 'login' => 'sessions#new', :as => :login
 
   resources :sessions
